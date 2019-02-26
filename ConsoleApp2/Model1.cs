6f25dba666cc42f8f2e930930d5a17691dbd85de
Helen Chen <42528353+kikychen69@users.noreply.github.com>
2019-02-27 01:10:06 +0800

    Add files via upload

---
+++ ConsoleApp2/Model1.cs
@@ -0,0 +1,65 @@
+namespace ConsoleApp2
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<UserCart> UserCarts { get; set; }
+        public virtual DbSet<UserTable> UserTables { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .Property(e => e.Title)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Book>()
+                .Property(e => e.ISBN)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Book>()
+                .Property(e => e.Author)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Category>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserTable>()
+                .Property(e => e.FirstName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserTable>()
+                .Property(e => e.LastName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserTable>()
+                .Property(e => e.PhoneNo)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserTable>()
+                .Property(e => e.PostalCode)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserTable>()
+                .Property(e => e.Address)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserTable>()
+                .Property(e => e.Email)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ ConsoleApp2/OrderDetail.cs
@@ -0,0 +1,22 @@
+namespace ConsoleApp2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class OrderDetail
+    {
+        [Key]
+        public int OrderID { get; set; }
+
+        public int BookID { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
